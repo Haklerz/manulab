f83fdeb0c1cc740a27de49cfbfdc177100a080b5
Haklerz <lervik.haakon@gmail.com>
2021-03-26 14:23:12 +0100

    Acheived login

--- src/main/java/no/ntnu/manulab/Main.java
+++ src/main/java/no/ntnu/manulab/Main.java
@@ -7,26 +7,36 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 
 public class Main {
 
     public static void main(String[] args) {
         try (CloseableHttpClient client = HttpClients.createDefault()) {
 
-            HttpGet get = new HttpGet("http://printer17.local" + "/api/login");
+            HttpPost post = new HttpPost("http://printer17.local" + "/api/login");
+            post.addHeader("X-Api-Key", "055A6B6898E74D9D83A65FBA4D8C2655");
+            post.setEntity(new StringEntity("{\"passive\": true}", ContentType.APPLICATION_JSON));
+
+            HttpGet get = new HttpGet("http://printer17.local" + "/api/printer");
             get.addHeader("X-Api-Key", "055A6B6898E74D9D83A65FBA4D8C2655");
-            
+
+            HttpPost home = new HttpPost("http://printer17.local" + "/api/printer/printhead");
+            home.addHeader("X-Api-Key", "055A6B6898E74D9D83A65FBA4D8C2655");
+            home.setEntity(new StringEntity("{\"command\": \"home\",\"axes\": [\"x\", \"y\"]}", ContentType.APPLICATION_JSON));
 
             try (CloseableHttpResponse response = client.execute(get)) {
 
                 System.out.println(response.getCode() + " " + response.getReasonPhrase());
-                HttpEntity entity = response.getEntity();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
+                HttpEntity body = response.getEntity();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(body.getContent()));
 
                 String line;
                 while (((line = reader.readLine()) != null)) {
@@ -34,7 +44,7 @@ public class Main {
                 }
                 // do something useful with the response body
                 // and ensure it is fully consumed
-                EntityUtils.consume(entity);
+                EntityUtils.consume(body);
 
             } catch (IOException e) {
                 e.printStackTrace();
