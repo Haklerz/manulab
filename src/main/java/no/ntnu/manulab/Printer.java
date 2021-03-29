f132d76e3e8d36a17763d03955c7ad4ee352b8bc
Haklerz <lervik.haakon@gmail.com>
2021-03-29 16:51:42 +0200

    Set up http method calling

---
+++ src/main/java/no/ntnu/manulab/Printer.java
@@ -0,0 +1,85 @@
+package no.ntnu.manulab;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.github.cliftonlabs.json_simple.JsonException;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
+
+import org.apache.hc.client5.http.classic.methods.HttpDelete;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPatch;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.classic.methods.HttpPut;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.Method;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+
+public class Printer implements Closeable {
+    public final String host, apiKey;
+    private final CloseableHttpClient httpClient;
+
+    public Printer(String host, String apiKey) {
+        this.host = host;
+        this.apiKey = apiKey;
+        this.httpClient = HttpClients.createDefault();
+    }
+
+    public String getHost() {
+        return this.host;
+    }
+
+    public JsonObject executeMethod(Method method, String path, JsonObject json) throws IOException, JsonException {
+        ClassicHttpRequest request = createRequest(method, URI.create(this.host + path));
+        request.addHeader("X-Api-Key", this.apiKey);
+        request.setEntity(new StringEntity(json.toJson(), ContentType.APPLICATION_JSON));
+
+        JsonObject response = null;
+        try (CloseableHttpResponse httpResponse = this.httpClient.execute(request)) {
+            HttpEntity body = httpResponse.getEntity();
+            InputStreamReader content = new InputStreamReader(body.getContent(), "UTF-8");
+
+            response = (JsonObject) Jsoner.deserialize(content);
+            EntityUtils.consume(body);
+        }
+
+        return response;
+    }
+
+    private ClassicHttpRequest createRequest(Method method, URI uri) {
+        switch (method) {
+        case DELETE:
+            return new HttpDelete(uri);
+
+        case GET:
+            return new HttpGet(uri);
+
+        case PATCH:
+            return new HttpPatch(uri);
+
+        case POST:
+            return new HttpPost(uri);
+
+        case PUT:
+            return new HttpPut(uri);
+
+        default:
+            return null;
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.httpClient.close();
+    }
+}
--- src/main/java/no/ntnu/manulab/Super.java
+++ src/main/java/no/ntnu/manulab/Super.java
@@ -1,5 +1,34 @@
 package no.ntnu.manulab;
 
-public class Super {
-    
+import java.util.HashSet;
+import java.util.Set;
+
+public class Super implements Runnable {
+
+    private Set<Printer> printers;
+
+    public Super() {
+        this.printers = new HashSet<>();
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+
+
+
+
+    }
+
+    public void addPrinter(Printer printer) {
+        this.printers.add(printer);
+    }
+
+    public void removePrinter(Printer printer) {
+        this.printers.remove(printer);
+    }
+
+    public void removePrinterByHost(String host) {
+        this.printers.removeIf(printer -> host.equals(printer.getHost()));
+    }
 }
