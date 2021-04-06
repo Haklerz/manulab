ca88d2142705f9673f811529e2a11f6a977665c2
Haklerz <lervik.haakon@gmail.com>
2021-04-06 15:34:55 +0200

    Fixed callMethod

--- src/main/java/no/ntnu/manulab/Printer.java
+++ src/main/java/no/ntnu/manulab/Printer.java
@@ -6,11 +6,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import com.github.cliftonlabs.json_simple.JsonException;
 import com.github.cliftonlabs.json_simple.JsonObject;
 import com.github.cliftonlabs.json_simple.Jsoner;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.hc.client5.http.classic.methods.HttpDelete;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPatch;
@@ -26,6 +29,9 @@ import org.apache.hc.core5.http.Method;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 
+/**
+ * Represents a 3D-printer
+ */
 public class Printer implements Closeable {
     public final String host, apiKey;
     private final CloseableHttpClient httpClient;
@@ -36,49 +42,63 @@ public class Printer implements Closeable {
         this.httpClient = HttpClients.createDefault();
     }
 
-    public String getHost() {
-        return this.host;
-    }
-
-    public JsonObject callMethod(Method method, String path, JsonObject json) throws IOException, JsonException, URISyntaxException {
+    public JsonObject callMethod(Method method, String path, JsonObject json) throws IOException, URISyntaxException {
         URI uri = new URI("http", this.host, path, null);
+
         ClassicHttpRequest request = createRequest(method, uri);
+        if (request == null) {
+            throw new IllegalArgumentException("Given method type is not valid");
+        }
+
         request.addHeader("X-Api-Key", this.apiKey);
         request.setEntity(new StringEntity(json.toJson(), ContentType.APPLICATION_JSON));
 
         JsonObject response = null;
         try (CloseableHttpResponse httpResponse = this.httpClient.execute(request)) {
-            HttpEntity body = httpResponse.getEntity();
+            HttpEntity entity = httpResponse.getEntity();
+            String body = IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8);
 
-            //InputStreamReader content = new InputStreamReader(body.getContent(), "UTF-8");
+            JsonObject defaultValue = new JsonObject().putChain("error", body);
 
-            //response = (JsonObject) Jsoner.deserialize(content);
-            EntityUtils.consume(body);
+            response = Jsoner.deserialize(body, defaultValue);
+
+            EntityUtils.consume(entity);
         }
 
         return response;
     }
 
+    /**
+     * Returns an instance of the corresponding request given a method type.
+     * 
+     * @param method
+     * @param uri
+     * @return
+     */
     private ClassicHttpRequest createRequest(Method method, URI uri) {
         switch (method) {
         case DELETE:
             return new HttpDelete(uri);
-
         case GET:
             return new HttpGet(uri);
-
         case PATCH:
             return new HttpPatch(uri);
-
         case POST:
             return new HttpPost(uri);
-
         case PUT:
             return new HttpPut(uri);
-
         default:
             return null;
         }
+    }
+
+    /**
+     * Returns the printers host adress.
+     * 
+     * @return the printers host adress
+     */
+    public String getHost() {
+        return this.host;
     }
 
     @Override
