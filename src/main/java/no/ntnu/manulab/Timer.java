f3dfc7c0dc3c4cf2f53aa6ab08350d2e1ebe3ecb
Haklerz <lervik.haakon@gmail.com>
2021-04-08 16:45:58 +0200

    Added some conveniance methods to Timer

--- src/main/java/no/ntnu/manulab/Timer.java
+++ src/main/java/no/ntnu/manulab/Timer.java
@@ -1,27 +1,43 @@
 package no.ntnu.manulab;
 
 public class Timer {
-    private long setTime;
+    private long recordedTime;
 
     public Timer() {
-        this.setTime = System.nanoTime();
+        this.recordedTime = System.nanoTime();
     }
 
     /**
-     * Sets a point in time relative to now.
+     * Records a point in time relative to now.
      * 
      * @param time the relative time in seconds
      */
     public void set(double time) {
-        this.setTime = System.nanoTime() + (long) (time * 1e9);
+        this.recordedTime = System.nanoTime() + (long) (time * 1e9);
     }
 
     /**
-     * Gets the relative time between now and the set time.
+     * Records the current time.
+     */
+    public void set() {
+        set(0);
+    }
+
+    /**
+     * Returns the relative time between now and the recorded time.
      * 
      * @return the relative time in seconds
      */
     public double get() {
-        return (System.currentTimeMillis() - this.setTime) * 1e-9;
+        return (System.currentTimeMillis() - this.recordedTime) * 1e-9;
+    }
+
+    /**
+     * Returns weather the recorded time has expired.
+     * 
+     * @return weather the time has expired
+     */
+    public boolean isExpired() {
+        return get() >= 0;
     }
 }
